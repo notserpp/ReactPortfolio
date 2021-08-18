2303995fa7f3f6b2baae1885f7f0d5b323586115
notserp <prestonsegura@yahoo.com>
2021-08-17 17:39:55 -0700

    working background and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar"
+import AboutMe from './components/AboutMe';
+import Background from './components/Background'
 
 function App() {
   return (
+    <div>
+   <Background />
    <Navbar />
+   <AboutMe />
+   </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,52 @@
 import React from 'react'
 import logo from "../logo.png"
+import $ from "jquery"
+
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/#"><img src={logo} className="logo" alt="logo..."></img></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/#">Link</a>
-                        </li>
-                    </ul>
-                </div>
-        </nav>
+            <nav className="autohide navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <img src={logo} className="logo" alt=""></img>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="main_nav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item"><a className="nav-link nav-link-fade-up" href="/#">About Me</a></li>
+                            <li className="nav-item"><a className="nav-link nav-link-fade-up" href="/#">Portfolio</a></li>
+                            <li className="nav-item"><a className="nav-link nav-link-fade-up" href="/#">Experience</a></li>
+                            <li className="nav-item"><a className="nav-link nav-link-fade-up" href="/#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>                
+            </nav>
+            
     )
 }
+
+
+$(document).ready(function () {
+    const el_autohide = document.querySelector('.autohide');
+    if (el_autohide) {
+        var last_scroll_top = 0;
+        window.addEventListener('scroll', function () {
+            let scroll_top = window.scrollY;
+            if (scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+        // window.addEventListener
+    }
+    // if
+
+
+})
 
 export default Navbar
